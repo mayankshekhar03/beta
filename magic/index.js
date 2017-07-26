fd30bd71970cbb9a0dd508eb0ed06617d66172ea
Mayank Shekhar <mayankshekhar03@gmail.com>
2017-07-26 22:41:03 +0530

    most of the container functionalities are working

--- magic/index.js
+++ magic/index.js
@@ -1,3 +1,22 @@
 $(document).ready(function(){
-    
+    $('.container').hide();
+    $('#project').click(function(){
+        $('#projects').addClass('animated zoomIn');
+        $('.container').show();
+        setTimeout(function(){$('#projects').removeClass('animated fadeIn');}, 1000);
+        $('#p1').addClass('selected');
+        $('.details .P1').siblings().hide();
+        $('.list').bind('click', function(event) {
+        var proid = '#' + event.target.id;
+        var proclass = '.' + event.target.id.toUpperCase();
+        $(proid).addClass('selected');
+        $(proid).siblings().removeClass('selected');
+        $('.details ' + proclass).show();
+        $('.details ' + proclass).siblings().hide();
+        
+    });
+    });
+    $('.back').click(function(){
+        $('.container').hide();
+    });
 });
